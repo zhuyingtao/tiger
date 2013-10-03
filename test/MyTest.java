54ed621ab06c56b6ba504a6edc61a59045a9b632
zyt6217315a <287501244@qq.com>
2013-10-04 01:21:11 +0800

    solution to lab2 partA

---
+++ test/MyTest.java
@@ -0,0 +1,18 @@
+class Factorial {
+	public static void main(String[] a) {
+		System.out.println(new Fac().ComputeFac(10));
+	}
+}
+
+class Fac {
+	public int ComputeFac(int num) {
+		int num_aux;
+		if (num < !!!!!!!1)
+			num_aux = 1;
+		else
+			num_aux = num * (this.ComputeFac(num - 1).length.length.length);
+		num = 1 + 2 * 3 + 5 * 7;
+
+		return num_aux;
+	}
+}
