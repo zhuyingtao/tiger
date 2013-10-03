d5b1b2f96f7ee0d9d6ee3fbfe07f6547900a2daf
zyt6217315a <287501244@qq.com>
2013-10-03 08:46:47 +0800

    merge lab1

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -3,67 +3,192 @@ package lexer;
 import java.io.InputStream;
 
 import util.Todo;
-
 import lexer.Token.Kind;
 
-public class Lexer
-{
-  String fname; // the input file name to be compiled
-  InputStream fstream; // input stream for the above file
+public class Lexer {
+	String fname; // the input file name to be compiled
+	InputStream fstream; // input stream for the above file
 
-  public Lexer(String fname, InputStream fstream)
-  {
-    this.fname = fname;
-    this.fstream = fstream;
-  }
+	int lineNum = 1;
+	int colNum = 1;
 
-  // When called, return the next token (refer to the code "Token.java")
-  // from the input stream.
-  // Return TOKEN_EOF when reaching the end of the input stream.
-  private Token nextTokenInternal() throws Exception
-  {
-    int c = this.fstream.read();
-    if (-1 == c)
-      // The value for "lineNum" is now "null",
-      // you should modify this to an appropriate
-      // line number for the "EOF" token.
-      return new Token(Kind.TOKEN_EOF, null);
+	public Lexer(String fname, InputStream fstream) {
+		this.fname = fname;
+		this.fstream = fstream;
+	}
 
-    // skip all kinds of "blanks"
-    while (' ' == c || '\t' == c || '\n' == c) {
-      c = this.fstream.read();
-    }
-    if (-1 == c)
-      return new Token(Kind.TOKEN_EOF, null);
+	String[] keyWords = { "boolean", "class", "else", "extends", "false", "if",
+			"int", "length", "main", "new", "out", "println", "public",
+			"return", "static", "String", "System", "this", "true", "void",
+			"while" };
 
-    switch (c) {
-    case '+':
-      return new Token(Kind.TOKEN_ADD, null);
-    default:
-      // Lab 1, exercise 2: supply missing code to
-      // lex other kinds of tokens.
-      // Hint: think carefully about the basic
-      // data structure and algorithms. The code
-      // is not that much and may be less than 50 lines. If you
-      // find you are writing a lot of code, you
-      // are on the wrong way.
-      new Todo();
-      return null;
-    }
-  }
+	// When called, return the next token (refer to the code "Token.java")
+	// from the input stream.
+	// Return TOKEN_EOF when reaching the end of the input stream.
+	private Token nextTokenInternal() throws Exception {
+		int c = this.fstream.read();
+		if (-1 == c)
+			// The value for "lineNum" is now "lineNum",
+			// you should modify this to an appropriate
+			// line number for the "EOF" token.
+			return new Token(Kind.TOKEN_EOF, lineNum, colNum);
 
-  public Token nextToken()
-  {
-    Token t = null;
+		// skip all kinds of "blanks"
+		while (' ' == c || '\t' == c || '\n' == c || '\r' == c) {
+			this.posChange(c);
+			c = this.fstream.read();
+		}
 
-    try {
-      t = this.nextTokenInternal();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    if (control.Control.lex)
-      System.out.println(t.toString());
-    return t;
-  }
+		if (-1 == c)
+			return new Token(Kind.TOKEN_EOF, lineNum, colNum++);
+
+		switch (c) {
+		case '+':
+			return new Token(Kind.TOKEN_ADD, lineNum, colNum++);
+
+			/******************* My Code ***********************/
+		case '=':
+			return new Token(Kind.TOKEN_ASSIGN, lineNum, colNum++);
+		case ',':
+			return new Token(Kind.TOKEN_COMMER, lineNum, colNum++);
+		case '.':
+			return new Token(Kind.TOKEN_DOT, lineNum, colNum++);
+		case '{':
+			return new Token(Kind.TOKEN_LBRACE, lineNum, colNum++);
+		case '[':
+			return new Token(Kind.TOKEN_LBRACK, lineNum, colNum++);
+		case '(':
+			return new Token(Kind.TOKEN_LPAREN, lineNum, colNum++);
+		case '<':
+			return new Token(Kind.TOKEN_LT, lineNum, colNum++);
+		case '!':
+			return new Token(Kind.TOKEN_NOT, lineNum, colNum++);
+		case '}':
+			return new Token(Kind.TOKEN_RBRACE, lineNum, colNum++);
+		case ']':
+			return new Token(Kind.TOKEN_RBRACK, lineNum, colNum++);
+		case ')':
+			return new Token(Kind.TOKEN_RPAREN, lineNum, colNum++);
+		case ';':
+			return new Token(Kind.TOKEN_SEMI, lineNum, colNum++);
+		case '-':
+			return new Token(Kind.TOKEN_SUB, lineNum, colNum++);
+		case '*':
+			return new Token(Kind.TOKEN_TIMES, lineNum, colNum++);
+		case '&':
+			c = this.fstream.read();
+			if (c == '&')
+				return new Token(Kind.TOKEN_ADD, lineNum, colNum++);
+			else {
+				// error
+				System.out.println("No Such Token: " + c + " at line "
+						+ lineNum + " , column " + colNum);
+				System.exit(0);
+			}
+
+		default:
+			// Lab 1, exercise 2: supply missing code to
+			// lex other kinds of tokens.
+			// Hint: think carefully about the basic
+			// data structure and algorithms. The code
+			// is not that much and may be less than 50 lines. If you
+			// find you are writing a lot of code, you
+			// are on the wrong way.
+			/******************* My Code ***********************/
+			// a. Is an Integer?
+			String s = (char) c + "";
+			this.fstream.mark(0);
+			if (c >= '0' && c <= '9') {
+				c = this.fstream.read();
+				while (c >= '0' && c <= '9') {
+					s += (char) c;
+					this.fstream.mark(0);
+					c = this.fstream.read();
+				}
+				this.fstream.reset();
+				int sColNum = colNum;
+				colNum = colNum + s.length();
+				return new Token(Kind.TOKEN_NUM, lineNum, sColNum, s);
+			}
+
+			// b. Is an Identifier?
+			if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+				c = this.fstream.read();
+				while (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0'
+						&& c <= '9' || c == '_') {
+					s += (char) c;
+					this.fstream.mark(0);
+					c = this.fstream.read();
+				}
+				this.fstream.reset();
+				int sColNum = colNum;
+				colNum = colNum + s.length();
+				// b.1 Is a KeyWord
+				for (int i = 0; i < keyWords.length; i++) {
+					if (s.equals(keyWords[i])) {
+						s = "TOKEN_" + s.toUpperCase();
+						return new Token(Kind.valueOf(s), lineNum, sColNum);
+					}
+				}
+				// b.2 Not a KeyWord
+				return new Token(Kind.TOKEN_ID, lineNum, sColNum, s);
+			}
+			// c. Comments?
+			if (c == '/') {
+				c = this.fstream.read();
+				// c.1 Comments"//"
+				if (c == '/') {
+					c = this.fstream.read();
+					while (c != '\n') {
+						c = this.fstream.read();
+					}
+					this.posChange(c);
+					return this.nextTokenInternal();
+				}
+				// c.2 Comments"/*"
+				if (c == '*') {
+					colNum += 2;
+					while (true) {
+						c = this.fstream.read();
+						this.posChange(c);
+						if (c == '*') {
+							c = this.fstream.read();
+							this.posChange(c);
+							if (c == '/')
+								break;
+						}
+					}
+					return this.nextTokenInternal();
+				}
+			}
+
+			new Todo();
+			return null;
+		}
+	}
+
+	public Token nextToken() {
+		Token t = null;
+
+		try {
+			t = this.nextTokenInternal();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		if (control.Control.lex)
+			System.out.println(t.toString());
+		return t;
+	}
+
+	public void posChange(int c) {
+		if (c == '\t')
+			colNum += 4;
+		else if ((c != '\n') && (c != '\r'))
+			colNum++;
+		else if (c == '\n') {
+			lineNum++;
+			colNum = 1;
+		}
+	}
 }
